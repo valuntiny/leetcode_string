8fffd4a841ae7cbbe57daf7778e308ef5d56e250
valuntiny <459201296@qq.com>
2018-10-15 00:25:02 -0400

    finished this problem, easy one

--- reverseString.py
+++ reverseString.py
@@ -2,3 +2,21 @@
 Write a function that takes a string as input and returns the string reversed.
 '''
 
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        temp = []
+        for i in range(len(s)):
+            temp.append(s[len(s) - 1 - i])
+
+        return (''.join(temp))
+
+x = "A man, a plan, a canal: Panama"
+
+test = Solution()
+
+print(test.reverseString(x))
